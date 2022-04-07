649b1421d53bb1a932c10d7af5dfcdf7865d7377
btruj007 <102835844+btruj007@users.noreply.github.com>
2022-04-07 09:59:22 -0700

    Update IntList.cpp

--- IntList.cpp
+++ IntList.cpp
@@ -1,23 +1,80 @@
 //
 // Created by keraw on 4/5/2022.
 //
-
+#include <iostream>
+#include <ostream>
 #include "IntList.h"
 
-IntList()
-: dummyHead(nullptr), dummyTail(nullptr) {}
+IntList::IntList(){
+    dummyHead = new IntNode(0);
+    dummyTail =new IntNode(0);
 
-~IntList(){
+    dummyHead->next = dummyTail;
+    dummyTail->prev = dummyHead;
+}
+
+IntList::~IntList(){
     if (dummyHead == nullptr && dummyTail == nullptr) {
         return;
     }
     
 }
-void push_front(int value);
-void pop_front();
-void push_back(int value);
-void pop_back();
-bool empty() const;
 
-friend ostream & operator<<(ostream &out, const IntList &rhs);
-void printReverse() const;
+void IntList::push_front(int value){        //works
+    IntNode* temp = new IntNode(value);
+    temp->next = dummyHead->next;
+    dummyHead->next->prev = temp;
+    temp->prev = dummyHead;
+    dummyHead->next = temp;
+}
+void IntList::pop_front(){                  //To do
+    std::cout << "WRITE ME: pop_front" << endl;
+}
+void IntList::push_back(int value)         //To do
+{
+    std::cout << "WRITE ME: push_back()" << endl;
+}
+void IntList::pop_back()                        //works
+{
+    if (dummyTail->prev != dummyHead)
+    {
+        IntNode* temp = dummyTail->prev;
+        dummyTail->prev = temp->prev;
+        temp->prev->next = dummyTail;
+        delete temp;
+        temp = nullptr;
+    }
+}
+bool IntList::empty() const {               //works
+    if (dummyHead->next == dummyTail)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+ostream& operator<<(ostream& out, const IntList& rhs)      //To do
+{
+    out << "WRITE ME: operator<<" << endl;
+    return out;
+}
+
+void IntList::printReverse() const {                //works
+    if (dummyTail->prev != nullptr)
+    {
+        IntNode* curr = dummyTail->prev;
+
+        while (curr != dummyHead)
+        {
+            cout << curr->data;
+
+            if (curr->prev != dummyHead)
+            {
+                cout << " ";
+            }
+
+            curr = curr->prev;
+        }
+    }
+}
