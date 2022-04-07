759af286ebbf3b50a074dd77324bc4986e0f2239
quacamole <102833311+quacamole@users.noreply.github.com>
2022-04-07 10:11:34 -0700

    cmakes

---
+++ cmake-build-debug/CMakeFiles/CS10CLab2.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/keraw/OneDrive/Documents/GitHub/CS10CLab2/.gitignore/IntList.cpp" "CMakeFiles/CS10CLab2.dir/IntList.cpp.obj" "gcc" "CMakeFiles/CS10CLab2.dir/IntList.cpp.obj.d"
+  "C:/Users/keraw/OneDrive/Documents/GitHub/CS10CLab2/.gitignore/main.cpp" "CMakeFiles/CS10CLab2.dir/main.cpp.obj" "gcc" "CMakeFiles/CS10CLab2.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CS10CLab2.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for CS10CLab2.
+# This may be replaced when dependencies are built.
